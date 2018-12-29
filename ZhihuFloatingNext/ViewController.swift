76f7dc602df1b0047bbf64be2763e9e2a5ee4b0d
iMoeNya <sym9688@outlook.com>
2018-12-29 11:58:47 +0800

    Initial Commit

---
+++ ZhihuFloatingNext/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZhihuFloatingNext
+//
+//  Created by 孙一萌 on 2018/12/29.
+//  Copyright © 2018 iMoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
